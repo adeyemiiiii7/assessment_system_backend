61672ebc41c62dffb93205151dabfb3472f0ad2a
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2024-04-25 15:47:04 +0100

    used the env file for db url protection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -6,9 +7,7 @@ const authRouter = require('./routes/auth');
 const adminRouter = require('./routes/admin');
 // Initialize Express app
 const app = express();
-const PORT = 8080; 
-const DB = 'mongodb+srv://adeyemi:TITIlope1970@cluster0.sjpo9ga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
+const PORT = 8080;
 
 app.use(cors())
 app.use(morgan('combined'))
@@ -18,24 +17,14 @@ app.use(authRouter);
 app.use(adminRouter);
 
 // Connect to MongoDB
-mongoose
-.connect(DB)
+mongoose.connect(process.env.DB)
 .then(() => {
-    console.log("Connection Successfull");
+    console.log("Connection Successful");
 })
 .catch((e) =>{
     console.log(e);
 })
 
-// app.get('/api/data', (req, res) => {
-//   // Handle your API logic here
-//   const data = { message: 'Hello from the server!' };
-//   res.json(data);
-// });
-
 app.listen(PORT, (req,res)=>{
   console.log(`Server is running on port ${PORT}...`)
 })
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
