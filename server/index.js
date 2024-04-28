8b2753cb97655d924627c71fa2e561638c8be2b0
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2024-04-28 21:51:55 +0100

    added my user router

--- server/index.js
+++ server/index.js
@@ -5,16 +5,10 @@ const morgan = require('morgan');
 const cors = require ("cors")
 const authRouter = require('./routes/auth');
 const adminRouter = require('./routes/admin');
+const userRouter = require('./routes/user');
 // Initialize Express app
 const app = express();
 const PORT = 8080;
-
-app.use(cors())
-app.use(morgan('combined'))
-//middleware
-app.use(express.json());
-app.use(authRouter);
-app.use(adminRouter);
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB)
@@ -24,6 +18,17 @@ mongoose.connect(process.env.DB)
 .catch((e) =>{
     console.log(e);
 })
+
+
+
+app.use(cors())
+app.use(morgan('combined'))
+//middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(userRouter);
+
 
 app.listen(PORT, (req,res)=>{
   console.log(`Server is running on port ${PORT}...`)
