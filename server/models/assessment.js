13b0bf2ac6a914f7258d171098342350191174c7
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2024-05-02 22:55:30 +0100

    changes

--- server/models/assessment.js
+++ server/models/assessment.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
-const {questionSchema} = require('../models/questions');
+const { questionSchema } = require('../models/questions');
+
 const assessmentSchema = mongoose.Schema({
   title: {
     type: String,
     required: true
   },
-
+  firstname: {
+    type: String,
+    required: true 
+  },
+  lastname: {
+    type: String,
+    required: true 
+  },
   questions: [questionSchema], 
-
   cancelled: {
     type: Boolean,
     default: false
   }
-  
-
 });
 
 const Assessment = mongoose.model('Assessment', assessmentSchema);
